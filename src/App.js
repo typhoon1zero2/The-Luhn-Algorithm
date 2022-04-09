c30c39d3070d62e5fc83ed8577c0a42601f084c4
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-08 22:33:23 -0400

    Created app function and import react hook

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css"
+import { useState, useEffect } from 'react';
+
+
+function App() {
+
+
+
+  return (
+    <div>
+        <h1>Luhn Algorithm</h1>
+        <h2>Credit Card Check</h2>
+    </div>
+  )
+}
+
+export default App
